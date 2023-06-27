c70aacfb6464e5bfa10d1583c7565bbc75243b33
Taylor Ambrose <taykesby@gmail.com>
2023-06-27 12:09:36 -0400

    more CSS changes to media queries

--- temp/src/components/Hero.jsx
+++ temp/src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
         <div className='hero' id='home'>
             <a href='https://www.linkedin.com/in/taylorambrose'><img src={imageURL} alt="logo" id='logo' /></a>
-            <h2>Full-Stack Software Engineer/Web Developer</h2>
+            <h2>Full-Stack Software Engineer / Web Developer</h2>
             <br />
             <h2 id="hello">Hello! My name is</h2>
             <h1>Taylor Ambrose</h1>
